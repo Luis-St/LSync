4ab716bdc57a4fb7e2ba4b4512dc1cfb5434347b
Luis <luis@staudt-fridingen.de>
2023-06-18 19:49:29 +0200

    added Font Awesome fonts

---
+++ src/main/java/net/luis/lsync/ui/Fonts.java
@@ -0,0 +1,16 @@
+package net.luis.lsync.ui;
+
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class Fonts {
+	
+	public static final Font FA_BRANDS = Font.loadFont(Fonts.class.getResourceAsStream("/font/font_awesome_brands.otf"), 12);
+	public static final Font FA_REGULAR = Font.loadFont(Fonts.class.getResourceAsStream("/font/font_awesome_regular.otf"), 12);
+	public static final Font FA_SOLID = Font.loadFont(Fonts.class.getResourceAsStream("/font/font_awesome_solid.otf"), 12);
+}
